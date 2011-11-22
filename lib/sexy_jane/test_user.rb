03c888ba3420d334c99127331d7d6332de5ac8b6
Jason Zhao <jasonzhao6@gmail.com>
2011-11-22 12:21:52 -0800

    acceptance test

--- lib/sexy_jane/test_user.rb
+++ lib/sexy_jane/test_user.rb
@@ -1,31 +1,39 @@
 require 'koala'
+require 'capybara'
 
 module SexyJane
-  class TestUser
-    
-    def self.test_users
-      @test_users ||= Koala::Facebook::TestUsers.new(:app_id => Facebook.connection.api_key, :secret => Facebook.connection.secret)
-    end
-    
-    def self.logged_in(permissions)
+  class TestUser    
+    def self.logged_in(permissions = nil)
       user = new(permissions)
+      user.login
       yield user
-      test_users.delete(user.id)
+      test_user_model.delete(user.id)
     end
     
-    def initialize(permissions)
-      @test_user = self.class.test_users.create(true, permissions)
-      #p @test_user
+    def self.test_user_model
+      @test_user_model ||= Koala::Facebook::TestUsers.new(:app_id => Facebook.connection.api_key, :secret => Facebook.connection.secret)
+    end
+    
+    def initialize(permissions = nil)
+      @test_user = self.class.test_user_model.create(true, permissions)
       api = Koala::Facebook::API.new(@test_user['access_token'])
       @me = api.get_object('me')
-      #p @me
     end
     
+    def login
+      Capybara.visit 'http://www.facebook.com'
+      Capybara.fill_in 'Email', :with => self.email
+      Capybara.fill_in 'Password', :with => self.password
+      Capybara.click_button 'Log In'
+    end
+        
     def method_missing(name)
       key = name.to_s
       return @test_user[key] if @test_user[key]
       return @me[key] if @me[key]
       super
     end
+    
+    private_class_method :new
   end
 end
--- spec/sexy_jane_spec.rb
+++ spec/sexy_jane_spec.rb
@@ -1,24 +1,46 @@
 require 'bundler/setup'
 require 'sexy_jane'
+require 'capybara/rspec'
+require 'yaml'
 
 describe SexyJane::Facebook do
   describe ".connect!" do
     before { SexyJane::Facebook.connect!('123', 'abc') }
-    subject { SexyJane::Facebook.connection }
-    its(:api_key) { should == '123' }
-    its(:secret) { should == 'abc' }
+    context "when connected with (123, abc)" do
+      describe ".connection" do
+        subject { SexyJane::Facebook.connection }
+        its(:api_key) { should == '123' }
+        its(:secret) { should == 'abc' }      
+      end
+    end
   end
 end
 
 describe SexyJane::TestUser do
-  describe ".logged_in" do    
-    # Usage:
-    #
-    # SexyJane::Facebook.connect!('123', 'abc')
-    # SexyJane::TestUser.logged_in('email') do |user|
-    #   fill_in "Name", :with => user.name
-    #   fill_in "Email", :with => user.email
-    # end
-    it "yields a user"
+  describe ".logged_in" do
+    
+  end
+end
+
+feature "Facebook Test Users", %q{
+  In order to test my Facebook canvas appliaction
+  As a developer
+  I want to automate test user creation and deletion
+} do
+  
+  background do
+    Capybara.default_driver = :selenium
+    config = YAML.load_file('spec/test.yml')
+    SexyJane::Facebook.connect!(config['api_key'], config['secret'])
+  end
+  
+  scenario "Wrap my testing code with SexyJane" do
+    SexyJane::TestUser.logged_in do |user|
+      visit 'http://www.facebook.com/'
+      page.should have_content(user.name)
+      fill_in 'Email Password:', :with => 'hello word'
+    end
+    visit 'http://www.facebook.com/'
+    page.should have_content('Sign Up')
   end
 end
